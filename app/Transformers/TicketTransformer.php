22638962d327dbda52dbc5feb1098a7e47a72cc3
Rick Bolton <rickbolton@outlook.com>
2017-07-19 15:34:26 +0100

    Added status and priority include to ticket transformer

--- app/Transformers/TicketTransformer.php
+++ app/Transformers/TicketTransformer.php
@@ -4,10 +4,17 @@ namespace App\Transformers;
 
 
 use App\Ticket;
+use App\Transformers\TicketPriorityTransformer;
+use App\Transformers\TicketStatusTransformer;
 use League\Fractal\TransformerAbstract;
 
 class TicketTransformer extends TransformerAbstract
 {
+    protected $defaultIncludes = [
+        'status',
+        'priority',
+    ];
+
     /**
      * A Fractal transformer.
      *
@@ -24,5 +31,30 @@ class TicketTransformer extends TransformerAbstract
             'status_id' => $ticket->status_id,
             'created_at' => $ticket->created_at->format('Y-m-d H:i:s'),
         ];
+    }
+
+    /**
+     * Include Status
+     *
+     * @return League\Fractal\ItemResource
+     */
+    public function includeStatus(Ticket $ticket)
+    {
+        $status = $ticket->status;
+
+        return $this->item($status, new TicketStatusTransformer);
+    }
+
+
+    /**
+     * Include Priority
+     *
+     * @return League\Fractal\ItemResource
+     */
+    public function includePriority(Ticket $ticket)
+    {
+        $priority = $ticket->priority;
+
+        return $this->item($priority, new TicketPriorityTransformer);
     }
 }
