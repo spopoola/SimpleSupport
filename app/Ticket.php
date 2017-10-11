c77aeb394342897b5bdf884a01cd251b9bf20afe
Rick Bolton <rickbolton@outlook.com>
2017-10-11 10:45:08 +0100

    Fix to ticket creation
    Added default status_id and priority_id. Priority needs to be selected on ticket creation. Updated store method to return TicketTransfomer and remove unused storeTicket within ticket actions.

--- app/Ticket.php
+++ app/Ticket.php
@@ -9,6 +9,13 @@ class Ticket extends Model
 {
     use FetchesTicketsByType;
 
+    const STATUS_OPEN = 1;
+    
+    protected $attributes = [
+        'status_id' => self::STATUS_OPEN,
+        'priority_id' => 1, //temp need to select this on ticket creation
+    ];
+
     protected $fillable = ['ticket_number', 'user_id', 'priority_id',
         'status_id', 'assigned_to', 'subject', 'content', 'is_overdue',
         'is_replied', 'is_closed', 'is_deleted'
